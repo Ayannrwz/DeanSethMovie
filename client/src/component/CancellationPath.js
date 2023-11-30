ee2ad08128f4bb12fba0761fe74268f30d0c31d6
wannaBeAyan <enriquez7adrian@gmail.com>
2023-11-30 22:54:57 +0800

    First Commit

---
+++ client/src/component/CancellationPath.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { Form, Select } from "antd";
+import {
+  CancelContainer,
+  Container,
+  SelectContainer,
+  CancelWallpaper,
+} from "../stylesheets/CssCancellation";
+import reservationNumbers from "./ReservationNumber";
+import { Button } from "antd";
+import { ArrowLeftOutlined } from "@ant-design/icons";
+import TicketTable from "./ticketTable";
+
+const { Option } = Select;
+
+const CancellationPath = () => {
+
+  return (
+    <Container>
+      <CancelWallpaper>
+        <div
+          style={{
+            paddingTop: "20px",
+            textAlign: "left",
+            marginLeft: "20px",
+          }}
+        >
+          {/* <Button
+            style={{
+              backgroundColor: "rgba(255, 255, 255, 0.816)",
+              width: "40px",
+            }}
+            type="default"
+            icon={<ArrowLeftOutlined />}
+            onClick={handleBackButtonClick}
+          ></Button> */}
+        </div>
+        <h1
+          style={{
+            paddingTop: "80px",
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
+          Cancellation
+        </h1>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <CancelContainer style={{ alignItems: "center" }}>
+            <TicketTable />
+          </CancelContainer>
+        </div>
+      </CancelWallpaper>
+    </Container>
+  );
+};
+
+export default CancellationPath;
